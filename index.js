8fc79a2f686b6160eaf1b45adf346241f35a0f18
Charlotte B <bec.charlotte@yahoo.fr>
2025-01-08 15:06:39 +0100

    modification de app.use(cors) et rajout de app.option

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   })
 );
+
+app.options("*", cors());
 
 // Middleware pour traiter le JSON
 app.use(express.json({ limit: "20mb" }));
